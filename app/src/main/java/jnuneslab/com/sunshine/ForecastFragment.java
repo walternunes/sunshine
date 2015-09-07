7230eec6006e7372d5eccc07f4e183451942deb2
Walter <jnunes.walter@gmail.com>
2015-09-06 22:02:01 -0300

    Adding refresh button for debuggin porpose. API request working correctly

--- app/src/main/java/jnuneslab/com/sunshine/ForecastFragment.java
+++ app/src/main/java/jnuneslab/com/sunshine/ForecastFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +28,13 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
     }
 
     private class FetchWeatherTask extends AsyncTask<URL, Integer, Long> {
@@ -75,6 +83,7 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
+                Log.e(TAG, "JSON Buffer ->" + forecastJsonStr);
             } catch (IOException e) {
                 Log.e(TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attempting
@@ -94,6 +103,17 @@ public class ForecastFragment extends Fragment {
             }
             return null;
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
